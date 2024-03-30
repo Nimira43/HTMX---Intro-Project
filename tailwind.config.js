c463d7c8ae4c297ac126ea7dd1913402328358ef
Nick Rabone <n.rabone72@aol.com>
2024-03-30 02:29:17 +0000

    Update

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ["Montserrat", "sans-serif"],
+        alata: ["Alata"]
+      }
+    },
+  },
+  plugins: [],
+}
+

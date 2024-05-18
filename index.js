77bc1d567cacd1a2e15cb49e5bf3ef617a790c3e
Nick Rabone <n.rabone72@aol.com>
2024-05-19 00:19:56 +0100

    Long Polling demo

--- index.js
+++ index.js
@@ -4,9 +4,21 @@ import { fileURLToPath } from 'url'
 
 const app = express()
 const PORT = 3000
+let count = 0 
 
 app.get('/', (req, res) => {
     res.sendFile(`${dirname(fileURLToPath(import.meta.url))}/index.html`)
+})
+
+app.get('/content', (req, res) => {
+    count++
+    if (count > 5) {
+        count = 0
+        return res.status(286)
+    }
+    // If statement will stop request from client 
+
+    res.send('<h2>Content from /content</h2>')
 })
 
 app.listen(PORT, () => {

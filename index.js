16c5e3b58d1cca1924838bbe232e7c387ecea111
Nick Rabone <n.rabone72@aol.com>
2024-05-19 01:41:14 +0100

    Tidied up code

--- index.js
+++ index.js
@@ -12,25 +12,11 @@ app.get('/', (req, res) => {
     res.sendFile(`${dirname(fileURLToPath(import.meta.url))}/index.html`)
 })
 
-//      LONG POLLING
-//
-app.get('/polling', (req, res) => {
-    count++
-    if (count > 5) {
-        count = 0
-        return res.status(286)
-    }
-    res.send('<h2>Content from /polling</h2>')
-})
-//
-//////////////////////////////////////////
-
 app.get('/indicator', (req, res) => {
     setTimeout(() => {
         res.send('<h2>Request Indicator</h2>')
     }, 3000)
 })
-
 
 app.listen(PORT, () => {
     console.log(`Server is listening on ${PORT}`)

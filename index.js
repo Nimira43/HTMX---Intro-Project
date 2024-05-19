8f32d13ef4ef52c840b07b0d8e369d48a46994f1
Nick Rabone <n.rabone72@aol.com>
2024-05-19 01:37:50 +0100

    Request Indicators demo

--- index.js
+++ index.js
@@ -4,22 +4,33 @@ import { fileURLToPath } from 'url'
 
 const app = express()
 const PORT = 3000
-let count = 0 
+// let count = 0 
+
+app.use(express.static(`${dirname(fileURLToPath(import.meta.url))}/public`))
 
 app.get('/', (req, res) => {
     res.sendFile(`${dirname(fileURLToPath(import.meta.url))}/index.html`)
 })
 
-app.get('/content', (req, res) => {
+//      LONG POLLING
+//
+app.get('/polling', (req, res) => {
     count++
     if (count > 5) {
         count = 0
         return res.status(286)
     }
-    // If statement will stop request from client 
-
-    res.send('<h2>Content from /content</h2>')
+    res.send('<h2>Content from /polling</h2>')
 })
+//
+//////////////////////////////////////////
+
+app.get('/indicator', (req, res) => {
+    setTimeout(() => {
+        res.send('<h2>Request Indicator</h2>')
+    }, 3000)
+})
+
 
 app.listen(PORT, () => {
     console.log(`Server is listening on ${PORT}`)
